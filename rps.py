8dd53562f47125a9789b2758689268b6d5f8ad1e
longnghts <82634370+longnghts@users.noreply.github.com>
2021-05-05 15:45:38 -0400

    updated code

--- rps.py
+++ rps.py
@@ -1,10 +1,15 @@
 import random
-
+playerScore=0
+cpuScore=0
+scoreLimit = 3
 while True: #=>  Copied the play again code from an online resource
 
     playerMove = input('What do you play? (rock, paper or scissors?):')
     choices = ['rock', 'paper', 'scissors']
     cpuMove = random.choice(choices)
+
+
+
 
     print(f'You chose {playerMove} and the computer chose {cpuMove}')
 
@@ -13,20 +18,36 @@ while True: #=>  Copied the play again code from an online resource
     elif playerMove == 'scissors':
         if cpuMove == 'rock':
             print('Rock beats scissors, YOU LOSE!')
+            cpuScore+= 1
+            print(playerScore, cpuScore)
         else:
             print('Scissors cut paper, YOU WIN!')
+            playerScore+= 1
+            print(playerScore, cpuScore)
     elif playerMove == 'rock':
         if cpuMove == 'paper':
             print('Paper covers rock, YOU LOSE!')
+            cpuScore+= 1
+            print(playerScore, cpuScore)
         else:
             print('Rock beats scissors, YOU WIN!')
+            playerScore+= 1
+            print(playerScore, cpuScore)
     elif playerMove == 'paper':
         if cpuMove == 'scissors':
             print('Scissors cut paper, YOU LOSE!')
+            cpuScore+= 1
+            print(playerScore, cpuScore)
         else:
             print('Paper covers rock, YOU WIN!')
+            playerScore+= 1
+            print(playerScore, cpuScore)
 
-
-    play_again = input('Play again? (y/n): ')#=>  Copied the play again code from an online resource
-    if play_again.lower() != "y":#=>  Copied the play again code from an online resource
-        break#=>  Copied the play again code from an online resource
+    if cpuScore >= 3 or playerScore >= 3:
+        play_again = input('Play again? (y/n): ')#=>  Copied the play again code from an online resource
+        if play_again.lower() != "y":#=>  Copied the play again code from an online resource
+            print('Game Over!')
+            break#=>  Copied the play again code from an online resource
+        else:
+            cpuScore = 0
+            playerScore = 0
